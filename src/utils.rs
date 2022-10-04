797011d162650c2d518adbc9010aa46e8ea1dd6d
amigin <amigin@gmail.com>
2022-10-04 13:11:45 +0300

    Some Improvments

--- src/utils.rs
+++ src/utils.rs
@@ -8,17 +8,18 @@ pub fn calc_pages_amount_to_ressize(position: usize, page_size: usize, pages_rat
     big_pages_amount * pages_rate
 }
 
-pub fn fill_content_to_comply_with_page_blob_size(content: &mut Vec<u8>, byte_to_fill: u8) {
+pub fn fill_content_to_comply_with_page_blob_size(
+    content: &mut Vec<u8>,
+    byte_to_fill: u8,
+    blob_page_size: usize,
+) {
     if content.len() == 0 {
         return;
     }
 
-    let pages_amount = get_pages_amount_by_size(
-        content.len(),
-        my_azure_storage_sdk::page_blob::consts::BLOB_PAGE_SIZE,
-    );
+    let pages_amount = get_pages_amount_by_size(content.len(), blob_page_size);
 
-    let required_size = pages_amount * my_azure_storage_sdk::page_blob::consts::BLOB_PAGE_SIZE;
+    let required_size = pages_amount * blob_page_size;
 
     while content.len() < required_size {
         content.push(byte_to_fill);
@@ -35,7 +36,7 @@ mod tests {
     fn test_page_blob_resize() {
         let mut content = vec![1u8, 2u8, 3u8];
 
-        fill_content_to_comply_with_page_blob_size(&mut content, 32u8);
+        fill_content_to_comply_with_page_blob_size(&mut content, 32u8, BLOB_PAGE_SIZE);
 
         assert_eq!(BLOB_PAGE_SIZE, content.len());
     }
@@ -44,7 +45,7 @@ mod tests {
     fn test_page_blob_resize_zero_content() {
         let mut content = vec![];
 
-        fill_content_to_comply_with_page_blob_size(&mut content, 32u8);
+        fill_content_to_comply_with_page_blob_size(&mut content, 32u8, BLOB_PAGE_SIZE);
 
         assert_eq!(0, content.len());
     }
@@ -53,7 +54,7 @@ mod tests {
     fn test_page_blob_resize_if_we_have_exactly_one_page() {
         let mut content = vec![0u8; 512];
 
-        fill_content_to_comply_with_page_blob_size(&mut content, 32u8);
+        fill_content_to_comply_with_page_blob_size(&mut content, 32u8, BLOB_PAGE_SIZE);
 
         assert_eq!(BLOB_PAGE_SIZE, content.len());
     }
