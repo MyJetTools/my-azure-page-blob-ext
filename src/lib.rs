95e55297eb65586341a8ee5a222ba9f6fb87074b
amigin <amigin@gmail.com>
2022-10-04 13:01:35 +0300

    Added fill_content_to_comply_with_page_blob_size

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,4 @@ mod page_blob_cached_data;
 mod pages_cache;
 pub use exact_payload::ExactPayload;
 pub use my_azure_page_blob_advanced::MyAzurePageBlobAdvanced;
-mod utils;
+pub mod utils;
--- src/utils.rs
+++ src/utils.rs
@@ -7,3 +7,54 @@ pub fn calc_pages_amount_to_ressize(position: usize, page_size: usize, pages_rat
     let big_pages_amount = crate::utils::get_pages_amount_by_size(position, big_page_size);
     big_pages_amount * pages_rate
 }
+
+pub fn fill_content_to_comply_with_page_blob_size(content: &mut Vec<u8>, byte_to_fill: u8) {
+    if content.len() == 0 {
+        return;
+    }
+
+    let pages_amount = get_pages_amount_by_size(
+        content.len(),
+        my_azure_storage_sdk::page_blob::consts::BLOB_PAGE_SIZE,
+    );
+
+    let required_size = pages_amount * my_azure_storage_sdk::page_blob::consts::BLOB_PAGE_SIZE;
+
+    while content.len() < required_size {
+        content.push(byte_to_fill);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use my_azure_storage_sdk::page_blob::consts::BLOB_PAGE_SIZE;
+
+    use super::fill_content_to_comply_with_page_blob_size;
+
+    #[test]
+    fn test_page_blob_resize() {
+        let mut content = vec![1u8, 2u8, 3u8];
+
+        fill_content_to_comply_with_page_blob_size(&mut content, 32u8);
+
+        assert_eq!(BLOB_PAGE_SIZE, content.len());
+    }
+
+    #[test]
+    fn test_page_blob_resize_zero_content() {
+        let mut content = vec![];
+
+        fill_content_to_comply_with_page_blob_size(&mut content, 32u8);
+
+        assert_eq!(0, content.len());
+    }
+
+    #[test]
+    fn test_page_blob_resize_if_we_have_exactly_one_page() {
+        let mut content = vec![0u8; 512];
+
+        fill_content_to_comply_with_page_blob_size(&mut content, 32u8);
+
+        assert_eq!(BLOB_PAGE_SIZE, content.len());
+    }
+}
